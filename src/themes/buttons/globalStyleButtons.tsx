ebf0f2f311a7df94929191fdfcdadcd3a1ed51d2
Sergio J Fernandez Marza <jeral.sergio@gmail.com>
2024-12-30 02:05:45 -0300

    refactor button style

--- src/themes/buttons/globalStyleButtons.tsx
+++ src/themes/buttons/globalStyleButtons.tsx
@@ -1,19 +1,19 @@
 // export type ButtonMode = 'outlined' | 'flat' | undefined;
 // export type ButtonMode = 'outlined' | 'text' | 'contained' | 'elevated' | 'contained-tonal' | undefined
 
-export const globalStyleButtons = {
+export const globalStyleButtonsColor = {
     primaryColor: '#9e0c14',
     secondaryColor: '#0e112d',
     accentDefault: '#ff4081'
-    
 }
+
 export const papersButtonVariables = {
     primaryButton: {
-        primary: globalStyleButtons.primaryColor,
-        accent: globalStyleButtons.accentDefault,
+        primary: globalStyleButtonsColor.primaryColor,
+        accent: globalStyleButtonsColor.accentDefault,
     },
     secondaryButton: {
-        primary: globalStyleButtons.secondaryColor,
-        accent: globalStyleButtons.accentDefault
+        primary: globalStyleButtonsColor.secondaryColor,
+        accent: globalStyleButtonsColor.accentDefault
     }
 };
