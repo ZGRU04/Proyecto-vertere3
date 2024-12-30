12b28f7038c29273743cdec55ca45dcd4138c831
jeral92 <jeral.sergio@gmail.com>
2024-12-30 02:31:44 -0300

    Add app.tsx to src file

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+	Text,
+	View,
+} from 'react-native';
+import Config from 'react-native-config';
+import { Button, PaperProvider } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { LoginScreen } from './modules/auth/presentation/screens/LoginScreen';
+
+export function App(): React.JSX.Element {
+	return (
+		<PaperProvider>
+			<SafeAreaView style={{flex:1,  backgroundColor: 'white'}}>
+				{/* <Text>Vertere 3</Text>
+				<Text>Entorno Actual: {Config.ENV}</Text>
+				<Text>API URL: {Config.API_URL}</Text>
+				<Button icon='pencil' >Hola mundo</Button>
+				<Icon name="magnify" size={24} color='black'></Icon> */}
+				<LoginScreen></LoginScreen>
+			</SafeAreaView>
+		</PaperProvider>
+	);
+}
