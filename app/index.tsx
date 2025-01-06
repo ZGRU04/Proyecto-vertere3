ba1d4027fc8ca3d7636a6519689cd9d686c8f2e3
sergioJ92 <jeral.sergio@gmail.com>
2025-01-05 22:30:12 -0300

    Clean imports

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,6 @@
-import { Link, Stack, useNavigation } from 'expo-router';
+import { useNavigation } from 'expo-router';
 import { useEffect } from 'react';
-import { Image, Text, View, StyleSheet } from 'react-native';
 import { LoginScreen } from '../src/modules/auth/presentation/screens/LoginScreen';
-import { Main } from '../src/Main';
-import { PaperProvider } from 'react-native-paper';
 
 export default function Index() {
   const navigation = useNavigation();
